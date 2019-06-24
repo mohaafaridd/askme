6f716ec7caff1c5e86ffadcf86678b080401b5ce
Mohammed <moha.khamis@outlook.com>
2019-06-24 21:16:13 +0200

    Init commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.listen(port, () => {
+  console.log(`Server is on port ${port}`);
+})
