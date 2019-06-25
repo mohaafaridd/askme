af82b96ee54a62077a2c6517cdad608b99e5f263
Mohammed <moha.khamis@outlook.com>
2019-06-25 14:33:50 +0200

    added reply controller and route

---
+++ backend/controllers/replies.js
@@ -0,0 +1,114 @@
+const Reply = require('../models/reply');
+
+const postReply = async (req, res) => {
+
+  try {
+
+    const reply = new Reply(
+      {
+        reply: req.body.reply,
+        question: req.params.question,
+        replier: req.user.id
+      }
+    );
+
+    await reply.save();
+
+    res.send({ success: true, message: 'Reply posted!', reply });
+
+  } catch (error) {
+
+    res.send({ success: false, message: 'Reply not posted!' });
+
+  }
+
+}
+
+const getReply = async (req, res) => {
+
+  try {
+
+    const reply = await Reply.findOne({ id: req.params.id });
+
+    if (!reply) {
+
+      throw new Error();
+
+    }
+
+    res.send({ success: true, message: 'Reply found!', reply });
+
+  } catch (error) {
+
+    res.send({ success: false, message: 'Reply not found!' });
+
+  }
+
+}
+
+const updateReply = async (req, res) => {
+
+  try {
+
+    if ((req.body.reply.trim()).length < 2) {
+      throw new Error();
+    }
+
+    const reply = await Reply.findOneAndUpdate(
+      {
+        id: req.params.id,
+        replier: req.user.id,
+        question: req.params.question
+      },
+      { reply: req.body.reply },
+      { new: true }
+    );
+
+    if (!reply) {
+
+      throw new Error();
+
+    }
+
+    res.send({ success: true, message: 'Reply edited!', reply });
+
+  } catch (error) {
+
+    res.send({ success: false, message: 'Reply not edited!' });
+
+  }
+
+}
+
+const deleteReply = async (req, res) => {
+
+  try {
+
+    const reply = await Reply.findOneAndDelete({
+      id: req.params.id,
+      replier: req.user.id,
+      question: req.params.question
+    });
+
+    if (!reply) {
+
+      throw new Error();
+
+    }
+
+    res.send({ success: true, message: 'Reply deleted!', reply });
+
+  } catch (error) {
+
+    res.send({ success: false, message: 'Reply not deleted!' });
+
+  }
+
+}
+
+module.exports = {
+  postReply,
+  getReply,
+  updateReply,
+  deleteReply
+}
---
+++ backend/routes/replies.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const auth = require('../middlewares/auth');
+
+const repliesController = require('../controllers/replies');
+
+const router = express.Router();
+
+router.post('/:question/create', auth, repliesController.postReply);
+
+router.get('/:question/:id', repliesController.getReply);
+
+router.patch('/:question/:id', auth, repliesController.updateReply);
+
+router.patch('/:question/:id', auth, repliesController.updateReply);
+
+router.delete('/:question/:id', auth, repliesController.deleteReply);
+
+module.exports = router;
