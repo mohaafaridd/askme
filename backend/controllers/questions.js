386f37db317f8e53d9418ed94f7a6c5e36ac30a8
Mohammed <moha.khamis@outlook.com>
2019-06-25 12:34:54 +0200

    added update and delete question routes

--- backend/controllers/questions.js
+++ backend/controllers/questions.js
@@ -4,7 +4,7 @@ const postQuestion = async (req, res) => {
 
   try {
 
-    const question = new Question(req.body);
+    const question = new Question({ question: req.body.question, asker: req.user.id });
 
     await question.save();
 
@@ -24,6 +24,12 @@ const getQuestion = async (req, res) => {
 
     const question = await Question.findOne({ id: req.params.id });
 
+    if (!question) {
+
+      throw new Error();
+
+    }
+
     res.send({ success: true, message: 'Question found!', question });
 
   } catch (error) {
@@ -34,7 +40,61 @@ const getQuestion = async (req, res) => {
 
 }
 
+const updateQuestion = async (req, res) => {
+
+  try {
+
+    if ((req.body.question.trim()).length < 2) {
+      throw new Error();
+    }
+
+    const question = await Question.findOneAndUpdate(
+      { id: req.params.id, asker: req.user.id },
+      { question: req.body.question },
+      { new: true }
+    );
+
+    if (!question) {
+
+      throw new Error();
+
+    }
+
+    res.send({ success: true, message: 'Question edited!', question });
+
+  } catch (error) {
+
+    res.send({ success: false, message: 'Question not edited!' });
+
+  }
+
+}
+
+const deleteQuestion = async (req, res) => {
+
+  try {
+
+    const question = await Question.findOneAndDelete({ id: req.params.id, asker: req.user.id });
+
+    if (!question) {
+
+      throw new Error();
+
+    }
+
+    res.send({ success: true, message: 'Question deleted!', question });
+
+  } catch (error) {
+
+    res.send({ success: false, message: 'Question not deleted!' });
+
+  }
+
+}
+
 module.exports = {
   postQuestion,
-  getQuestion
+  getQuestion,
+  updateQuestion,
+  deleteQuestion
 }
--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
+const auth = require('../middlewares/auth');
+
 const questionsController = require('../controllers/questions');
 
 const router = express.Router();
 
-router.post('/create', questionsController.postQuestion);
+router.post('/create', auth, questionsController.postQuestion);
 
 router.get('/:id', questionsController.getQuestion);
+
+router.patch('/:id', auth, questionsController.updateQuestion);
+
+router.delete('/:id', auth, questionsController.deleteQuestion);
 
 module.exports = router;
