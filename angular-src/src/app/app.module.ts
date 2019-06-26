01e0c54d6f3f67033fb8655263ef3e277512a315
Mohammed <moha.khamis@outlook.com>
2019-06-26 18:10:18 +0200

    added login and register components

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { CustomMaterialModule } from './app-material-module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
