07557306ff3179f3a2fb49e7e55bbd468d6f6a8e
Mohammed <moha.khamis@outlook.com>
2019-08-12 14:14:54 +0200

    project sturcture changed

--- app.js
+++ app.js
@@ -22,13 +22,19 @@ app.use(cookieParser());
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'frontend')));
-
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(connectRoutes());
+// app.use(connectRoutes());
+
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+// ...
+// Right before your app.listen(), add this:
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
 
 server.listen(port, () => {
   console.log(`Server is on port ${port}`);
